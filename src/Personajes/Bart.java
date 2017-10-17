f6dfb6c8869c0ab805a36e6a51f740149fea454f
Gino <nico_rodri94@hotmail.com>
2017-10-16 23:13:00 -0300

    aparece algunos

--- src/Personajes/Bart.java
+++ src/Personajes/Bart.java
@@ -18,15 +18,15 @@ public class Bart extends Aliado {
 		rango=3;
 		costo=10;
 		
-		ImageIcon img = new ImageIcon(this.getClass().getResource("/sprites/homero/homeroStill.gif"));
+		ImageIcon img = new ImageIcon(this.getClass().getResource("/sprites/bart/bartStill.gif"));
 		
 		this.imagen = new JLabel();
 		this.imagen.setIcon(img);
 		this.imagen.setBounds(0, 0, 75, 75);
 		
 		this.imActual = new HashMap<String, Icon>();
-		this.imActual.put(Personaje.still_key,new ImageIcon("/sprites/homero/homeroStill.gif"));
-		this.imActual.put(Personaje.shoot_key,new ImageIcon("/sprites/homero/homeroAttack.gif"));
+		this.imActual.put(Personaje.still_key,new ImageIcon("/sprites/bart/bartStill.gif"));
+		this.imActual.put(Personaje.shoot_key,new ImageIcon("/sprites/bart/bartAttack.gif"));
 
 	
 	}
