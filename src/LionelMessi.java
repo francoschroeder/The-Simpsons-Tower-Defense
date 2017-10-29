f9875db9d15f337876f1333808ccdbe5be0ba8ab
Gino <nico_rodri94@hotmail.com>
2017-10-29 17:37:27 -0300

    bala

--- src/LionelMessi.java
+++ src/LionelMessi.java
@@ -7,7 +7,7 @@ import Juego.Juego;
 public class LionelMessi {
 	public static void main(String[] args){
 		GUIPrincipal gui = new GUIPrincipal();
-		Juego j = new Juego(gui);
+		Juego j = new Juego(gui.getPanelMapa());
 		Market market = new Market(j);
 		HiloDisparo disparo = new HiloDisparo(gui); 
 		HiloEnemigos h = new HiloEnemigos(j, gui,disparo);
