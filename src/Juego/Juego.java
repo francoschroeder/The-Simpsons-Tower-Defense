6e9aa78335cc895765784515266f04dc3f1d59ff
Gino <nico_rodri94@hotmail.com>
2017-11-04 17:54:17 -0300

    Revert "monedas"
    This reverts commit a68fceedb377de67d7644f09b775bedb7ccb27c4.

--- src/Juego/Juego.java
+++ src/Juego/Juego.java
@@ -112,8 +112,6 @@ public class Juego {
 		synchronized (enemigosActivos) {enemigosActivos.remove(p);}
 		matriz[(int) p.getPosicion().getX()][(int) p.getPosicion().getY()] = null;
 		gui.getMarket().sumarMonedas(p.serEliminado());
-		gui.actualizarCartel();
-		
 		
 	}
 	
