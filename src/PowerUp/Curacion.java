1e6daecdfadc0a279264b3dd0a80a174251ae5eb
Gino97 <nico_rodri95@hotmail.com>
2017-11-04 17:49:51 -0300

    powerUp listos

--- src/PowerUp/Curacion.java
+++ src/PowerUp/Curacion.java
@@ -5,4 +5,8 @@ public class Curacion extends PowerUp {
 	public Curacion() {
 		super("/sprites/powerUP/bomba.png");
 	}
+	
+	public VisitorPowerUp factory() {
+		return new VisitorCuracion();
+	}
 }
--- src/PowerUp/VisitorCuracion.java
+++ src/PowerUp/VisitorCuracion.java
@@ -4,22 +4,22 @@ import Juego.Aliado;
 import Juego.Enemigo;
 import Juego.Objeto;
 
-public class VisitorBomba extends VisitorPowerUp {
+public class VisitorCuracion extends VisitorPowerUp {
 
-	public VisitorBomba() {
+	public VisitorCuracion() {
 		
 	}
 	
 	public void afectar(Aliado a) {
-		a.setVidaActual(a.getVidaActual()-15);
+		a.setVidaActual(a.getVidaMax());;
 	}
 	
 	public void afectar(Enemigo e) {
-		e.setVidaActual(e.getVidaActual()-15);
+		
 	}
 	
 	public void afectar(Objeto o) {
-		
+		o.setVidaActual(o.getVidaMax());
 	}
 
 }
