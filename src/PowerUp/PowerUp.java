0170aa83e14eac7ccf81e6453be38a5254ae36eb
Gino <nico_rodri94@hotmail.com>
2017-11-13 12:25:44 -0300

    csacas

--- src/PowerUp/PowerUp.java
+++ src/PowerUp/PowerUp.java
@@ -6,6 +6,10 @@ import javax.swing.JButton;
 import GUI.GUIPrincipal;
 
 public abstract class PowerUp extends JButton {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	VisitorPowerUp visitor;
 	public PowerUp(String a){
 		super(new ImageIcon(GUIPrincipal.class.getResource(a)));
